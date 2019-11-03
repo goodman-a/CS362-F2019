c30a42538047d844725734c4c742abddf292082e
goodman-a <goodmaal@oregonstate.edu>
2019-11-03 05:34:17 -0600

    randomtestcard1 edits

--- projects/goodmaal/dominion/randomtestcard1.c
+++ projects/goodmaal/dominion/randomtestcard1.c
@@ -72,6 +72,12 @@ int AssertTest(int pass, char* msg)
 
 
 /* -- Helper Function Prototypes -- */
+void HandGenerator(struct gameState *state, int player, int size, int min, int max);
+void DisplayHand(struct gameState *state, int player, char* msg);
+void DisplayDiscard(struct gameState *state, int player, char* msg);
+void DisplayDeck(struct gameState *state, int player, char* msg);
+int HandCardCount(struct gameState *state, int player, int choice1);
+int HandCardCount2(struct gameState *state, int player, int choice1, int handPos);
 
 
 
@@ -81,7 +87,7 @@ int main(int argc, char** argv){
   /* -- Variables for Comparison, Checks, and Vericiations -- */
 
   // general iterators
-  int i, r;
+  int i, j, r;
 
   // gameplay values
   int handPos = 0, choice1 = 0, num_bys = 0, bonus = 0, bonus_start = 0;
@@ -93,35 +99,62 @@ int main(int argc, char** argv){
   int player = 0;
   int num_players, num_hand;
   
-
-  // MAX_DECK ; MAX_HAND ; 
-
   // Counters
   //int counter_success, counter_failure;
-  int count_tests = 50;
+  int count_tests = 500;
 
   // Kingdom Cards
   int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
 
 
-  // don't forget to free gamestate ... ooopsy
+  // Create a Game State and Clear Memory
+  struct gameState state;
+  memset(&state, 0, sizeof(struct gameState));   // clear the game state
 
-  /* -- For Loop -- */
+  /* -- Run count_tests number of tests -- */
+  for(i=0; i<count_tests; i++)
+  {
 
-    // randomize number of playes
-    num_players = rand() % 4;
+    printf("TEST #%d\n",i+1);
+    // fail flag
+    int failFlag = 0;
 
-    // Randomize hand size
-    int hand_size = rand()% MAX_HAND;
+    // randomize number of playes: 2 to 4 playes 
+    num_players = (rand()%(4-2+1))+2;
 
-    // Randomize what is in hand?
+    // Initialize Game
+    initializeGame(num_players, k, seed, &state);
+
+    // Randomize hand size & Position 
+    int hand_size = rand()% 10; //MAX_HAND;
+    handPos = rand()%hand_size;
+
+    // set game state (hand count, random generated hand, and place of playing card (baron))
+    state.handCount[player] = hand_size;
+    HandGenerator(&state, player, hand_size, 0, treasure_map);
+    state.hand[player][handPos] = baron; 
+    bonus = 0; // reset bonus
 
     // Randomize Estate Supply Count
+    int estate_count = state.supplyCount[estate];
+    state.supplyCount[estate] = rand()%(estate_count+1);  // may need to weight the zero value higher ...
+    //estate_count = state.supplyCount[estate];
 
-    // Randomize Discard count?
+    // Display All Randomly Generated Stuffss....
+    
+    printf("Number of Players: %d\n", num_players);
+    printf("Hand Size: %d & Hand Pos: %d\n", hand_size, handPos);
+    DisplayHand(&state, player, "Player1");
+    int hand_estate = HandCardCount(&state, player, estate);
+    printf("Number of Estates in Hand: %d vs. Current Estate Supply Count: %d vs. Original Estate Supply Count: %d\n",hand_estate, state.supplyCount[estate], estate_count);
+  
 
+    // reset the state
+   memset(&state, 0, sizeof(struct gameState));   // clear the game state
 
-
+  } // end of primary for-loop
+  
+ 
   return 0;
 
 }
@@ -129,6 +162,85 @@ int main(int argc, char** argv){
 
 /* -- Helper Functions -- */
 
+// Random Hand Generator 
+
+void HandGenerator(struct gameState *state, int player, int size, int min, int max)
+{
+  int i;
+  if(min > max)
+  {
+      int temp = min;
+      min = max;
+      max = temp;
+  }
+
+  for(i=0; i<size; i++)
+  {
+      state->hand[player][i] = (rand()%(max-min+1))+min;
+  }
 
 
+  return;
+}
 
+
+// Display Cards in Hand, Discard, and Deck Piles
+void DisplayHand(struct gameState *state, int player, char* msg)
+{
+    int m;
+    printf("%s Hand Pile: ", msg);
+    for (m=0; m<state->handCount[player]; m++) {
+        printf("(%d)", state->hand[player][m]);
+    }
+    printf("\n");
+}
+
+void DisplayDiscard(struct gameState *state, int player, char* msg)
+{
+    int m;
+    printf("%s Discard Pile: ", msg);
+    for (m=0; m<state->discardCount[player]; m++) {
+        printf("(%d)", state->discard[player][m]);
+    }
+    printf("\n");
+}
+
+void DisplayDeck(struct gameState *state, int player, char* msg)
+{
+    int m;
+    printf("%s Deck Pile: ", msg);
+    for (m=0; m<state->deckCount[player]; m++) {
+        printf("(%d)", state->deck[player][m]);
+    }
+    printf("\n");
+}  
+
+// Count of a specific card in hand.
+int HandCardCount(struct gameState *state, int player, int card)
+{
+    int count = 0;
+    int i;
+
+    for (i = 0; i < state->handCount[player]; i++)
+    {
+        if (state->hand[player][i] == card) count++;
+    }
+
+    return count;
+}
+
+// count of duplicate cards matching specific card.
+int HandCardCount2(struct gameState *state, int player, int choice1, int handPos)
+{
+    int count = 0;
+    int i;
+    for (i = 0; i < state->handCount[player]; i++)
+    {
+        if (i != handPos && state->hand[player][i] == state->hand[player][choice1] && i != choice1) 
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
